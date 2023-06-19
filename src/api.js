9a854e00f666f16826aa6f7e1fb60c38cf3ea5b3
Ahmed Haj Ahmed <ahajahmed@haverford.edu>
2023-06-19 12:11:16 -0400

    init commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+
+// const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6bf722c2bamsh52b4c78ce3654aap156ea7jsnfcd175397baa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "024afe52d96ec5f925a67c9b455efe47";
+
